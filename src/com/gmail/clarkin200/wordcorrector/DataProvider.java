fce41d528cf1f27c4e7dcf4e8297faea68981ca7
Renat <clarkin200@gmail.com>
2024-09-12 14:51:58 +0300

    Finalized Word Corrector Homework

---
+++ src/com/gmail/clarkin200/wordcorrector/DataProvider.java
@@ -0,0 +1,7 @@
+package com.gmail.clarkin200.wordcorrector;
+
+public class DataProvider {
+    public String[] getData() {
+        return new String[]{"brange", "plum", "tomato", "onibn", "grape"};
+    }
+}
