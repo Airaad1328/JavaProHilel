8bacb31126e0ea9158fc7daa77429d753b6e5ca2
Renat <clarkin200@gmail.com>
2024-10-06 13:29:28 +0300

    Finalized Multithreading Fruits Homework

---
+++ src/com/gmail/clarkin200/multithreadingfruits/DataRepository.java
@@ -0,0 +1,7 @@
+package com.gmail.clarkin200.multithreadingfruits;
+
+public class DataRepository {
+    public String[] getData() {
+        return new String[]{"orange", "apple", "plum", "mango"};
+    }
+}
